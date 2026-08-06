81753b004a01b4b7b69dc848342fe07a2e287e89
Tim Schneider <timschneider.xyz@gmail.com>
2026-08-06 17:16:19 +0200

    fixed vault hook, secured ssh validation and container file permissions

--- git-vault-hook.sh
+++ git-vault-hook.sh
@@ -2,17 +2,23 @@
 
 # Script that installs a Git pre-commit hook that prevents an unencrypted Ansible Vault file from being committed.
 
-if [ -d .git/ ]; then
+if [ ! -d .git/ ]; then
+  echo "Not a Git repository."
+  exit 1
+fi
+
 [ -f .git/hooks/pre-commit ] && rm .git/hooks/pre-commit
-cat <<EOT >> .git/hooks/pre-commit
-if ( cat vars/vault.yaml | grep -q "\$ANSIBLE_VAULT;" ); then
-  echo "\033[0;32mVault encrypted.\033[0m"
+
+cat <<'EOT' > .git/hooks/pre-commit
+#!/bin/sh
+
+if grep -q '^\$ANSIBLE_VAULT;' vars/vault.yaml 2>/dev/null; then
+  printf '\033[0;32mVault encrypted.\033[0m\n'
 else
-  echo "\033[0;31mVault not encrypted!\033[0m"
+  printf '\033[0;31mVault not encrypted!\033[0m\n'
   exit 1
 fi
 EOT
-fi
 
 chmod u+x .git/hooks/pre-commit
 echo "Ansible Vault Pre-Commit Hook installed."
